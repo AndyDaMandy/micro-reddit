5c81df0ca09e3502427d1a4fd94620485cd55136
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-07-25 20:37:17 -0400

    building models

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :username, :presence => true, :uniqueness => true
+    validates :password, :presence => true, :length => { :minimum => 5 }
+    has_many :posts
+end
